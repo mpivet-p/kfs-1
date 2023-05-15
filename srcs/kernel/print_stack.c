6e8646d971527fa2cf5128ceeeee9f2d2b832abf
Maxime Pivet-Pierre <mpivet-p@student.42.fr>
2023-05-15 21:39:24 +0000

    Working on stack thing

--- srcs/kernel/print_stack.c
+++ srcs/kernel/print_stack.c
@@ -10,28 +10,22 @@ void	print_stack(void)
 
 	asm ("movl %%ebp,%0" : "=r"(ebp));
 	asm ("movl %%esp,%0" : "=r"(esp));
-	while (ebp != 0)
+	while (*ebp != 0)
 	{
-		eip = ebp + 1;
-		printk("eip: 0x%x\n", eip);
-		printk("ebp: ox%x\n",  ebp);
-		printk("*ebp: ox%x\n",  *ebp);
-		printk("esp: ox%x\n",  esp);
-
-		dump_stack(ebp);
 		ebp = (uint32_t*)*ebp;
 	}
+	dump_stack(esp, ebp);
 }
 
-void	dump_stack(uint32_t *ebp)
+void	dump_stack(uint32_t *begin, uint32_t *end)
 {
-	uint8_t* ptr = ebp + 2;
+	uint8_t	*ptr = begin;
 
 	printk("Stack content:");
-	while (ebp >= ptr)
+	while (ptr <= end)
 	{
 		printk(" %x", *ptr);
-		ebp--;
+		ptr++;
 	}
 	printk("\n");
 }
