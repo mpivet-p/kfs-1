22c2b1e267c0bfa22b147a65b4571bd61018a0b3
Maxime Pivet-Pierre <mpivet-p@student.42.fr>
2023-05-28 12:01:48 +0000

    Stack dump is ok. Trying to add stack trace with function names

--- srcs/kernel/print_stack.c
+++ srcs/kernel/print_stack.c
@@ -1,9 +1,19 @@
 #include <stdint.h>
 #include "printk.h"
 
+#define LOW_NIBBLE(x) x & 0x0F
+#define HIGH_NIBBLE(x) (x >> 4) & 0x0F
+
+struct stackframe
+{
+	struct stackframe	*ebp;
+	uint32_t			eip;
+}
+
 void	print_stack(void)
 {
 	int b = 0x42424242;
+	struct stackframe *stk;
 	uint32_t	*ebp;
 	uint32_t	*esp;
 	uint32_t	*eip;
@@ -20,12 +30,20 @@ void	print_stack(void)
 void	dump_stack(uint32_t *begin, uint32_t *end)
 {
 	uint8_t	*ptr = begin;
+	char	conv[] = "0123456789abcdef";
 
-	printk("Stack content:");
+	puts("Stack content:\n");
 	while (ptr <= end)
 	{
-		printk(" %x", *ptr);
+		if ((uint32_t)ptr % 8 == 0)
+		{
+			printk("%p:", ptr);
+		}
+		printk(" %c%c", conv[HIGH_NIBBLE(*ptr)], conv[LOW_NIBBLE(*ptr)]);
+		if ((uint32_t)ptr % 8 == 7 || ptr == end)
+		{
+			puts("\n");
+		}
 		ptr++;
 	}
-	printk("\n");
 }
